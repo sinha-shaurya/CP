7957ac4a48ec0200068f58fcac9ec7b640b980e7
Shaurya Sinha <shaurya.sinha@protonmail.com>
2021-08-17 23:44:30 +0530

    Knapsack better DP from last index

--- knapsack.cpp
+++ knapsack.cpp
@@ -6,21 +6,21 @@
 using namespace std;
 int dp[1000][1000];
 
-//start from 0th index solution with DP
-int max_profit(int *cost, int *profit, int n, int capacity, int i)
+//start from last index
+int max_profit(int *cost, int *profit, int n, int capacity)
 {
-    if (i >= n)
+    if (n == 0 || capacity == 0)
         return 0;
     //check if solution exists in dp array
-    if (dp[capacity][i] != 0)
-        return dp[capacity][i];
+    if (dp[capacity][n] != 0)
+        return dp[capacity][n];
 
     int ans = 0;
-    if (cost[i] > capacity)
-        ans = max_profit(cost, profit, n, capacity, i + 1);
+    if (cost[n - 1] > capacity)
+        ans = max_profit(cost, profit, n, capacity);
     else
-        ans = max(profit[i] + max_profit(cost, profit, n, capacity - cost[i], i + 1), max_profit(cost, profit, n, capacity, i + 1));
-    dp[capacity][i] = ans;
+        ans = max(profit[n - 1] + max_profit(cost, profit, n - 1, capacity - cost[n - 1]), max_profit(cost, profit, n - 1, capacity));
+    dp[capacity][n] = ans;
     return ans;
 }
 
@@ -44,7 +44,8 @@ int main()
         cin >> profit[i];
     int capacity;
     cin >> capacity;
-
-    cout << max_profit(cost, profit, n, capacity, 0) << endl;
+    //setup dp array
+    memset(dp, 0, 1000 * 1000 * sizeof(int));
+    cout << max_profit(cost, profit, n, capacity) << endl;
     return 0;
 }
